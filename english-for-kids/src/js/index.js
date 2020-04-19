eabfccdf62798e4013c0be49ff88a2d36f1fca9c
Dzianis Subach <suboch.denny@gmail.com>
2020-04-19 19:02:42 +0300

    fix: fix console error

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -20,7 +20,7 @@ const blurBody = document.createElement('div');
 const createIcon = (result) => {
     const correctAnswer = document.createElement('img');
     correctAnswer.src = result ? 'img/correct-answer.png' : 'img/wrong-answer.png';
-    correctAnswer.classList.add('answer',);
+    correctAnswer.classList.add('answer', );
     const statPanel = document.querySelector('.stat-panel');
     statPanel.prepend(correctAnswer);
 }
@@ -30,10 +30,9 @@ header.addEventListener('click', event => {
         header.querySelector('.menu-button').classList.toggle('menu-button--rotate');
         header.querySelector('.desktop-menu').classList.toggle('desktop-menu--active');
         if (!document.querySelector('.blur-body')) {
-        blurBody.classList.add('blur-body');
-        document.body.append(blurBody);
-        }
-        else {
+            blurBody.classList.add('blur-body');
+            document.body.append(blurBody);
+        } else {
             blurBody.classList.remove('blur-body');
             blurBody.remove();
         }
@@ -41,7 +40,7 @@ header.addEventListener('click', event => {
         isGameStart = false;
         createMainContent(event.target.textContent, isPlayMode);
         header.querySelector('.menu-button').classList.toggle('menu-button--rotate');
-        header.querySelector('.desktop-menu').classList.toggle('desktop-menu--active');      
+        header.querySelector('.desktop-menu').classList.toggle('desktop-menu--active');
         currentPage = event.target.textContent;
         blurBody.remove();
     }
@@ -50,7 +49,7 @@ header.addEventListener('click', event => {
 
         isPlayMode = event.target.closest('input').checked;
         const statPanel = document.querySelector('.stat-panel');
-        statPanel.innerHTML='';
+        statPanel.innerHTML = '';
         if (!isPlayMode && isGameStart) isGameStart = false;
         playMode(currentPage, isPlayMode);
 
@@ -78,16 +77,9 @@ mainContent.addEventListener('click', event => {
                 if (event.target.closest('.card-word').getAttribute('data-audio-src') === cardsAudio[cardsAudio.length - 1]) {
                     event.target.classList.add('card-word--correct-answer');
                     playAudio('audio/correct.mp3');
-
-                    // const correctAnswer = document.createElement('img');
-                    // correctAnswer.src = 'img/correct-answer.png';
-                    // correctAnswer.classList.add('answer', 'answer--correct');
-                    // const statPanel = document.querySelector('.stat-panel');
-                    // statPanel.append(correctAnswer);
                     createIcon(true);
-
                     cardsAudio.pop();
-                    setTimeout(() => playAudio(cardsAudio[cardsAudio.length - 1]), 1000)
+                    if (cardsAudio.length>0) setTimeout(() => playAudio(cardsAudio[cardsAudio.length - 1]), 1000);     
                     if (!cardsAudio.length) {
                         const modalWindow = document.createElement('div');
                         modalWindow.classList.add('modal-window', );
@@ -98,8 +90,8 @@ mainContent.addEventListener('click', event => {
                         if (errorsCounter) {
                             modalDescription.textContent = `You have ${errorsCounter} errors!`;
                             modalImage.src = 'img/failure.jpg'
-
                             setTimeout(() => playAudio('audio/failure.mp3'), 500)
+
                         } else {
                             modalDescription.textContent = 'You Win!';
                             modalImage.src = 'img/success.jpg'
@@ -107,7 +99,7 @@ mainContent.addEventListener('click', event => {
                         }
                         modalWindow.append(modalDescription, modalImage);
                         document.body.append(modalWindow);
-                        setTimeout(() => modalWindow.remove(), 3500);
+                       setTimeout(() => modalWindow.remove(), 3500);
                         isGameStart = false;
                         currentPage = 'Main Page';
                         createMainContent(currentPage, true);
@@ -148,8 +140,6 @@ blurBody.addEventListener('click', () => {
     blurBody.remove();
 })
 
-
-
 mainContent.addEventListener('mouseout', event => {
     const card = document.querySelector('.card--rotate');
     if (!card) return;
@@ -159,5 +149,3 @@ mainContent.addEventListener('mouseout', event => {
     }
 
 })
-
-
